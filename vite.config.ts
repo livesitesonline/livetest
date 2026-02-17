8a90c8090f6af3467b697fc64d43725ac6e8f10f
livesitesonline <supernovacorts852@gmail.com>
2026-02-17 19:50:42 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // 👈 ADICIONE ESTA LINHA
-
+  base: "./", // 👈 adicione esta linha
   server: {
     host: "::",
     port: 8080,
@@ -14,12 +13,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
-
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
